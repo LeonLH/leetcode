c3a4e84b8b3a79a616e7f56845bf4680ae347f13
LeonLH <laohan1221@126.com>
2018-10-15 17:45:01 +0800

    101 1H S | update

---
+++ symmetricTree.cpp
@@ -0,0 +1,67 @@
+// 1. The result of Inorder traversal and Reverse-Inorder traversal are equal.
+// 2. The key of the problem is not only the val of node should be equal, but also the pointer should be symmetric.
+// 3. BUT inorder and preorder can define an unique tree, so 'inorder equals to reInorder' PLUS 'preorder equals to rePreorder' can make sure it is symmetric. 
+// There must be a better way for this problem. 
+
+
+
+#include<vector>
+#include<iostream>
+using namespace std;
+struct TreeNode{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x): val(x), left(NULL), right(NULL){}
+};
+class Solution{
+public:
+	void inorder(TreeNode* T, vector<int> &V);
+	void reInorder(TreeNode* T, vector<int> &RV);
+	void preorder(TreeNode* T, vector<int> &V);
+	void rePreorder(TreeNode* T, vector<int> &RV);
+	bool isSymmetric(TreeNode* root){
+		vector<int> inorderV, inorderRV;
+		vector<int> preorderV, preorderRV;
+		inorder(root, inorderV);
+		reInorder(root, inorderRV);
+		preorder(root, preorderV);
+		rePreorder(root, preorderRV);
+		if(inorderV == inorderRV && preorderV == preorderRV)
+			return true;
+		else
+			return false;
+	}
+};
+void Solution::preorder(TreeNode* T, vector<int> &V){
+	if(T){
+		V.push_back(T->val);
+		preorder(T->left, V);
+		preorder(T->right, V);
+	}	
+}
+void Solution::rePreorder(TreeNode* T, vector<int> &V){
+	if(T){
+		V.push_back(T->val);
+		rePreorder(T->right, V);
+		rePreorder(T->left, V);
+	}	
+}
+void Solution::inorder(TreeNode* T, vector<int> &V){
+	if(T){
+		inorder(T->left, V);
+		V.push_back(T->val);
+		inorder(T->right, V);
+	}	
+}
+void Solution::reInorder(TreeNode* T, vector<int> &RV){
+	if(T){
+		reInorder(T->right, RV);
+		RV.push_back(T->val);
+		reInorder(T->left, RV);
+	}
+}
+int main(){
+
+	return 0;
+}
